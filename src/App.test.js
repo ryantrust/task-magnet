467fc1dc930bf3f5ca1c6317485c3d32afeebf4d
Ryan Trust <ryantrust@ucla.edu>
2023-10-28 18:30:25 -0700

    Fixed ESLint warnings

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,7 @@
+// noinspection ES6UnusedImports
+/* eslint-disable no-unused-vars */
 import { render, screen } from '@testing-library/react';
 import App from './App';
+/* eslint-enable no-unused-vars */
 
-test.skip('No tests', () => 1);
+test.skip('No tests', () => {});
