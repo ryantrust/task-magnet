1568d01fc48745a65c8109c27f54f8f056c92adb
Arul Mathur <arulmathur1@gmail.com>
2023-11-30 18:05:34 -0800

    Landing page

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React from "react";
+import heroImage from "../assets/hero.png";
 
 export default () => (
-    <div>
-        Home Page
+  <body>
+    <nav id="header" className="w-full z-30 top-0 text-black mr-5">
+      <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+        <div className=" pl-4 flex items-center">
+          <a
+            className="toggleColour text-black no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+            href="#"
+          >
+            TaskMagnet
+          </a>
+        </div>
+        <div
+          className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+          id="nav-content"
+        >
+          <ul className="list-reset lg:flex justify-end flex-1 items-center">
+            <li className="mr-3">
+              <p className="pr-5 text-black">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                The best productivity platform for college students.
+              </p>
+            </li>
+            <li className="mr-3"></li>
+            <li className="mr-3"></li>
+          </ul>
+        </div>
+      </div>
+      <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
+    </nav>
+    <div className="pt-24">
+      <div className="container px-3 mx-auto flex flex-wrap items-center">
+        <div className="w-full md:w-2/5 flex justify-center items-start text-center md:text-left mb-5 md:mb-0">
+          <div>
+            <p className="uppercase tracking-loose w-full">Born at UCLA</p>
+            <h1 className="my-4 text-5xl font-bold leading-tight">
+              Tired of tabbing between Google Calendar, notes, and an alarm to
+              keep yourself organized?
+            </h1>
+            <p className="leading-normal text-2xl mb-2">
+              You won't have to for long. Join TaskMagnet today.
+            </p>
+            <div className="md:justify-start flex justify-center">
+              <a
+                href="#"
+                className="mx-2 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Sign up
+              </a>
+              <a
+                href="#"
+                className="mx-2 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Login
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-3/5 py-6 text-center">
+          <img className="w-auto md:w-4/5 z-50" src={heroImage} alt="Hero" />
+        </div>
+      </div>
     </div>
+  </body>
 );
