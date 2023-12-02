05f9ce0f4f5471995d1134679f031567e2930ffc
ahuang100 <andyhuang715@gmail.com>
2023-12-01 18:23:52 -0800

    backend server running on port 4000 (env file modified), edit mongoose schemas in router.js

--- api-server/server.js
+++ api-server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: "../.env" });
-
+const router = require('./routes/router');
 const app = express();
 
-app.use(cors());
+//listening to port 3000 for incoming requests 
+//integrate Auth0 authentication services 
+
+const corsOptions = {
+    origin: '*', 
+    credentials: true, 
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use('/', router);
+
+const port = 4000 
+const server = app.listen(port, () => {
+    console.log('Backend server is running on port ${port}')
+})
+
+
+
+
