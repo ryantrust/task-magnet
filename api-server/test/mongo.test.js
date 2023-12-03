0b07b340ff91d4335ed1abb24e2d91c579527198
Ryan Trust <ryantrust@ucla.edu>
2023-12-02 19:20:50 -0800

    Added tests for mongo handler

---
+++ api-server/test/mongo.test.js
@@ -0,0 +1,13 @@
+const mongoHandler = require("../mongo");
+const {ObjectId} = require("mongodb");
+const assert = require('assert');
+
+it('Initialize database', () => mongoHandler.initialize()).timeout(5 * 1000);
+
+it('Get task', () => {
+    mongoHandler.getTasksFromUser(new ObjectId('656aa4f54d05409988700467')).then(results => {
+        assert.ok(results.some(result => result._id === new ObjectId('656be3bd1d74cf029391341b')));
+    });
+});
+
+it('Close connection', mongoHandler.close);
