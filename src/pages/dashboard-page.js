e660eba1d765c3009f4116100793f3a979e7dd52
Arul Mathur <arulmathur1@gmail.com>
2023-12-04 22:57:30 -0800

    Better dashboard layout

--- src/pages/dashboard-page.js
+++ src/pages/dashboard-page.js
@@ -3,58 +3,63 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Dashboard = () => {
   const { logout } = useAuth0();
+
   const handleLogout = () => {
     logout({ returnTo: window.location.origin });
   };
 
   const handleProfileClick = () => {
+    // Redirect to the profile page
     window.location.href = "/profile";
   };
 
   return (
-    <div className="flex flex-col h-screen">
+    <div>
       {/* Header Bar */}
-      <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
-        <div className="cursor-pointer" onClick={handleProfileClick}>
-          {/* Replace with profile icon */}
-          <img
-            src="path/to/profile-icon.png"
-            alt="Profile"
-            className="w-6 h-6 rounded-full"
-          />
+      <div className="p-8 flex justify-between items-center bg-blue-300">
+        <div className="flex items-center">
+          {/* Profile Icon */}
+          <div
+            className="w-8 h-8 bg-gray-500 rounded-full cursor-pointer"
+            onClick={handleProfileClick}
+          >
+            {/*add an icon here */}
+          </div>
+          <span className="ml-2">Username</span>
         </div>
+        {/* Logout Button */}
         <button
+          className="bg-red-500 text-white px-4 py-2 rounded"
           onClick={handleLogout}
-          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
         >
           Logout
         </button>
       </div>
 
-      {/* Dashboard Content */}
-      <div className="flex-grow p-4 flex">
-        {/* Todo List */}
-        <div className="flex-1 bg-gray-200 p-4 m-2">
-          <h2 className="text-lg font-semibold mb-4">Todo List</h2>
-          {/* Add Todo List content here */}
-        </div>
-
-        {/* Calendar View */}
-        <div className="flex-1 bg-gray-200 p-4 m-2">
-          <h2 className="text-lg font-semibold mb-4">Calendar View</h2>
-          {/* Add Calendar content here */}
-        </div>
-
-        {/* Pomodoro Timer */}
-        <div className="flex-1 bg-gray-200 p-4 m-2">
-          <h2 className="text-lg font-semibold mb-4">Pomodoro Timer</h2>
-          {/* Add Pomodoro Timer content here */}
-        </div>
-
-        {/* Option 4 */}
-        <div className="flex-1 bg-gray-200 p-4 m-2">
-          <h2 className="text-lg font-semibold mb-4">Option 4</h2>
-          {/* Add Option 4 content here */}
+      {/* Dashboard Section */}
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        {/* Dashboard Selections */}
+        <div className="grid grid-cols-2 gap-4 w-full max-w-screen-xl mx-auto">
+          {/* Todo List */}
+          <div className="flex items-center justify-center h-64 p-8 bg-blue-500 rounded cursor-pointer">
+            <h1>Todo List</h1>
+            {/* Add content for Todo List */}
+          </div>
+          {/* Calendar View */}
+          <div className="flex items-center justify-center h-64 p-8 bg-green-500 rounded cursor-pointer">
+            <h1>Calendar View</h1>
+            {/* Add content for Calendar View */}
+          </div>
+          {/* Pomodoro Timer */}
+          <div className="flex items-center justify-center h-64 p-8 bg-yellow-500 rounded cursor-pointer">
+            <h1>Pomodoro Timer</h1>
+            {/* Add content for Pomodoro Timer */}
+          </div>
+          {/* Option 4 */}
+          <div className="flex items-center justify-center h-64 p-8 bg-purple-500 rounded cursor-pointer">
+            <h1>Option 4</h1>
+            {/* Add content for Option 4 */}
+          </div>
         </div>
       </div>
     </div>
